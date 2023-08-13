589be828dd13da190b7f860adde4ae7c14095c85
juan18506 <juan185067@gmail.com>
2023-08-12 22:47:40 -0300

    codigo estructurado en funciones y añadida closure para animacion de delay

--- js/products.js
+++ js/products.js
@@ -1,31 +1,63 @@
 'use strict'
 
-const AUTOS_ENDPOINT = 'https://japceibal.github.io/emercado-api/cats_products/101.json'
 const ul = document.getElementById('products')
+const spanCategory = document.getElementById('category')
 
-async function recuperarDatosDeAutos () {
-  const res = await fetch(AUTOS_ENDPOINT)
-  const data = await res.json()
-  return data.products
+function getProductsEndpoint () {
+  const productsEndpoint = `${PRODUCTS_URL}101${EXT_TYPE}`
+  return productsEndpoint
 }
 
+async function getProductsData (productsEndpoint) {
+  const productsData = await getJSONData(productsEndpoint)
+  return productsData
+}
 
-let delay_entrada = 25
+function delayEntranceAnimationClosure () {
+  let delayEntranceAnimation = 0
 
-recuperarDatosDeAutos()
-  .then(autos => {
-    autos.forEach(auto => {
-      ul.innerHTML += `
-        <li class="ul__li" style="animation-delay: ${delay_entrada}ms">
-          <img class="ul__img" src=${auto.image} alt="Auto">
-          <div class="ul__div">
-            <span class="ul__span ul__span--title">${auto.name}<span class="ul__span ul__span--precio">${auto.cost} ${auto.currency}</span></span>
-            <span class="ul__span">${auto.description}</span>
-            <span class="ul__span ul__span--vendidos">${auto.soldCount} vendidos</span>
-          </div>
-        </li>
-      `
+  return function () {
+    delayEntranceAnimation += 25
+    return delayEntranceAnimation
+  }
+}
 
-      delay_entrada += 25
-    })
-  })
+function addProductToHtml (product, getDelayEntranceAnimation) {
+  const { name, description, image, cost, currency, soldCount } = product
+  const delayEntranceAnimation = getDelayEntranceAnimation()
+
+  ul.innerHTML += `
+      <li class="ul__li" style="animation-delay: ${delayEntranceAnimation}ms">
+        <img class="ul__img" src=${image} alt=${name}>
+        <div class="ul__div">
+          <span class="ul__span ul__span--title">${name}<span class="ul__span ul__span--cost">${cost} ${currency}</span></span>
+          <span class="ul__span">${description}</span>
+          <span class="ul__span ul__span--soldcount">${soldCount} vendidos</span>
+        </div>
+      </li>
+    `
+}
+
+async function showProducts () {
+  const productsEndpoint = getProductsEndpoint()
+  const productsData = await getProductsData(productsEndpoint)
+  const { status, data } = productsData
+
+  if (status === 'error') {
+    ul.innerHTML += `<li class="ul__li">${data}</li>`
+    return
+  }
+
+  const { catName, products } = data
+  spanCategory.innerHTML += catName
+
+  if (products.length === 0) {
+    ul.innerHTML += '<li class="ul__li">No hay productos de esta categoría</li>'
+    return
+  }
+
+  const getDelayEntranceAnimation = delayEntranceAnimationClosure()
+  products.forEach(product => addProductToHtml(product, getDelayEntranceAnimation))
+}
+
+showProducts()
