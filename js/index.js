aa3dc21b32262fe06bfae94f2b4e5fe2601af41b
juan18506 <juan185067@gmail.com>
2023-09-11 18:15:02 -0300

    added domcontentloaded to index

--- js/index.js
+++ js/index.js
@@ -1,15 +1,17 @@
-document.getElementById('autos').addEventListener('click', () => {
-  localStorage.setItem('catID', 101);
-  location.href = 'products.html';
-});
-
-document.getElementById('juguetes').addEventListener('click', () => {
-  localStorage.setItem('catID', 102);
-  location.href = 'products.html';
-});
-
-document.getElementById('muebles').addEventListener('click', () => {
-  localStorage.setItem('catID', 103);
-  location.href = 'products.html';
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('autos').addEventListener('click', () => {
+    localStorage.setItem('catID', 101);
+    location.href = 'products.html';
+  });
+  
+  document.getElementById('juguetes').addEventListener('click', () => {
+    localStorage.setItem('catID', 102);
+    location.href = 'products.html';
+  });
+  
+  document.getElementById('muebles').addEventListener('click', () => {
+    localStorage.setItem('catID', 103);
+    location.href = 'products.html';
+  });
 });
 
