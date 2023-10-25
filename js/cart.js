a6b661a277e583ffff97289ddad10d22189bdf68
santidelelli <santidelelli1@gmail.com>
2023-10-25 00:07:15 -0300

    desafiate + cart.html responsive

--- js/cart.js
+++ js/cart.js
@@ -16,29 +16,35 @@ const showCartInfo = (cartInfo) => {
 		const { name, unitCost, currency, image, count } = product;
 
 		cartSection.innerHTML += `
-			<div class="col-md-2">
+			<div class="col-lg-2 col-4  border-bottom d-flex align-items-center pb-2">
 				<img class="w-75" src="${image}" alt="${name}">
 			</div>
-			<div class="col-md-2">
+			<div class="col-lg-3 col-4 border-bottom d-flex align-items-center">
 				<p>${name}</p>
 			</div>
-			<div class="col-md-2">
+			<div class="col-lg-2 col-4 border-bottom d-flex align-items-center">
 				<p>${currency}<span class="cost">${unitCost}</span></p>
 			</div>
-			<div class="col-md-2">
-				<input type="number" name="cantidad" class="form-control w-50 cantidad" value="${count}">
+			<div class="col-lg-2 col-5 border-bottom border-black d-flex align-items-center">
+				<input type="number" name="cantidad" min="1" class="form-control w-75 cantidad mb-3" value="${count}">
 			</div>
-			<div class="col-md-2">
+			<div class="col-lg-2 col-5 border-bottom border-black d-flex align-items-center">
 				<p><strong>${currency}<span class="subtotal">${unitCost * count}</span></strong><p>
 			</div>
-			<div class="col-md-2"></div>
+			<div class="col-lg-1 col-2 border-bottom border-black d-flex align-items-center justify-content-center pb-3">
+			    <i class="fas fa-trash-alt fa-lg delete-button" style="color: #cb1f23"></i>
+			</div>
 		`
 	});
 };
 
 document.addEventListener('DOMContentLoaded', async () => {
 	const cartInfo = await getCartInfo()
-	showCartInfo(cartInfo);
+	if (localStorage.getItem('cart') == "[]") {
+		document.getElementById('articulos').innerHTML = '<h1 class="text-center"> El carrito está vacío </h1>'
+	} else {
+	    showCartInfo(cartInfo);
+    } 
 
 	const quantityInputs = document.querySelectorAll('.cantidad');
 	const costSpans = document.querySelectorAll('.cost');
@@ -61,4 +67,18 @@ document.addEventListener('DOMContentLoaded', async () => {
 			localStorage.setItem('cart', JSON.stringify(localCartInfo));
 		});
 	}
+    
+	function removeCartProduct() {
+		let products = JSON.parse(localStorage.getItem('cart'));
+		let productName = event.currentTarget.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.innerText;
+		products = products.filter(product => product.name !== productName);
+		localStorage.setItem('cart', JSON.stringify(products));
+		location.reload();
+	};
+
+	const deleteButton = document.querySelectorAll('.delete-button');
+
+	for (let i = 0; i < deleteButton.length; i++) {
+		deleteButton[i].addEventListener('click', removeCartProduct);
+	};
 });
