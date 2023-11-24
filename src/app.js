f3533819881e4f1ae0c6eec8db61db0c8a83ef2e
Juan Alves Pedreira <juan185067@gmail.com>
2023-11-24 17:27:53 -0300

    req 3

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const port = 3000;
+const SECRET_KEY = 'contraseña';
 
 const loginRouter = require('./routes/loginRouter');
 const userCartRouter = require('./routes/userCartRouter');
@@ -13,7 +15,6 @@ const productsCommentsRouter = require('./routes/productsCommentsRouter');
 const cartRouter = require('./routes/cartRouter');
 
 app.use(express.json());
-
 app.use(express.static('src/public'));
 
 app.use('/login', loginRouter);
@@ -23,6 +24,15 @@ app.use('/api/sell', sellRouter);
 app.use('/api/cats_products', catsProductsRouter);
 app.use('/api/products', productInfoRouter);
 app.use('/api/products_comments', productsCommentsRouter);
+app.use('/api/cart', (req, res, next) => {
+  try {
+    const decoded = jwt.verify(req.headers['access-token'], SECRET_KEY);
+    console.log(decoded);
+    next();
+  } catch (err) {
+    res.status(401).json({ message: 'Usuario no autorizado' });
+  }
+});
 app.use('/api/cart', cartRouter);
 
 app.listen(port, () => {
